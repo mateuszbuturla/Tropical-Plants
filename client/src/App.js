5ebe92717838cc2da7117b4bc274e3481d9a9101
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-02-01 21:05:33 +0100

    create nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import config from './config';
 
+import Nav from './view/Nav/Nav';
 import Home from './view/Home/Home';
 import PlantsList from './view/PlantsList/PlantsList';
 import Footer from './view/Footer/Footer';
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Switch>
           <Route path="/:type" component={(props) => <PlantsList {...props} config={config} />} exact />
           <Route path="/" component={(props) => <Home {...props} config={config} />} exact />
---
+++ client/src/view/Nav/Nav.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import magnifier from '../../img/magnifier.png';
+
+import './nav.sass';
+
+class Nav extends React.Component {
+
+    state = {
+        showMobileMenu: false,
+        search: ''
+    }
+
+    handleInputChange(e) {
+        this.setState({ search: e.target.value });
+    }
+
+    toggleMobileMenu() {
+        const { showMobileMenu } = this.state;
+        this.setState({ showMobileMenu: !showMobileMenu });
+    }
+
+    offMobileMenu() {
+        this.setState({ showMobileMenu: false });
+    }
+
+    searchSubmit(e) {
+        e.preventDefault();
+    }
+
+    render() {
+        const { showMobileMenu, search } = this.state;
+        return (
+            <nav className="nav">
+                <div className="nav__top">
+                    <p className="nav__logo">Tropical Plants</p>
+                    <button className="nav__burger-menu-button" onClick={this.toggleMobileMenu.bind(this)}>
+                        X
+                    </button>
+                </div>
+                <ul className={`nav__link-list${showMobileMenu === true ? ' nav__link-list--active' : ''}`}>
+                    <li className="nav__link-container"><NavLink className="nav__link" activeClassName="nav__link--active" to="/" onClick={this.offMobileMenu.bind(this)} exact>Home</NavLink></li>
+                    <li className="nav__link-container"><NavLink className="nav__link" activeClassName="nav__link--active" to="/flowerpot" onClick={this.offMobileMenu.bind(this)}>Rośliny doniczkowe</NavLink></li>
+                    <li className="nav__link-container"><NavLink className="nav__link" activeClassName="nav__link--active" to="/garden" onClick={this.offMobileMenu.bind(this)}>Rośliny ogrodowe</NavLink></li>
+                    <li className="nav__link-container"><NavLink className="nav__link" activeClassName="nav__link--active" to="/login" onClick={this.offMobileMenu.bind(this)}>Logowanie</NavLink></li>
+                    <li className="nav__search">
+                        <form onSubmit={this.searchSubmit.bind(this)}>
+                            <input className="nav__search-input" type="text" onChange={this.handleInputChange.bind(this)} value={search} placeholder="Szukaj" />
+                            <button className="nav__search-button" type="submit">
+                                <img className="nav__search-icon" src={magnifier} alt=" wyszukiwarka lupa" />
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
